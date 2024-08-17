5d214f2d58358ffce1da6dce215ca27933b05da8
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2024-08-17 17:01:07 +0600

    api link changed

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const baseApi = createApi({
     reducerPath: 'baseApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/v1' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://futureskills-backend.vercel.app/api/v1' }),
     endpoints: () => ({}),
     tagTypes: ["carts","products"],
 })
